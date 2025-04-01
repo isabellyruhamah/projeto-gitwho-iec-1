92c07d28359775bc6aefb161f547733f133c98e5
isabellyruhamah <isabellyruhamah@gmail.com>
2025-04-01 10:29:50 -0300

    Conclusão de conteúdos dos repositórios

--- index.js
+++ index.js
@@ -1,26 +1,65 @@
 const input = document.querySelector('.input')
-const button = document.querySelector('.button')
+const form = document.querySelector('.form')
 
-const infosFn = async(nome)=>{
-    try{
+const infosFn = async (nome) => {
+    try {
         const resposta = await fetch(`https://api.github.com/users/${nome}`)
         const data = await resposta.json()
-        console.log(data)
-    }catch(err){
-        console.log(err)
-    }
-}
-const reposFn = async(nome)=>{
-    try{
-        const resposta = await fetch(`https://api.github.com/users/${nome}/repos`)
-        const data = await resposta.json()
-        console.log(data)
-    }catch(err){
+        let ctx = document.createElement('div')
+        ctx = `
+            <div class="ctx">
+                <div>
+                    <img src="${data.avatar_url}">
+                    <h2>${data.name}</h2>
+                    <p>${data.login}</p>
+                </div>
+                <div>
+                    <p>Bio: ${data.bio}</p>
+                    <p>Seguidores: ${data.followers} | Seguindo: ${data.following}</p>
+                    <p>Repositórios públicos: ${data.public_repos}</p>
+                    <a href="${data.html_url}" target="_blank">Acesse o perfil</a>
+                </div>
+            </div>
+        `
+        document.querySelector('.infos').innerHTML = ctx
+        console.log(data.name)
+    } catch (err) {
         console.log(err)
     }
 }
 
-button.addEventListener('click', ()=>{
-    document.querySelector('.infos').innerHTML = input
+const reposFn = async (nome) => {
+    try {
+        const resposta = await fetch(`https://api.github.com/users/${nome}/repos`)
+        const data = await resposta.json()
+
+        let ctx = ''
+        data.forEach((repo) => {
+            ctx += `
+                <div class = "repo">
+                  <h2>${repo.name}</h2>
+                  <hr>
+                  <p>Permite fork${repo.allow_forking}
+                  </p>
+                  <p>URL para clone ${repo.clone_URL}</p>
+                  <p>Criado em: ${repo.create_at}</p>
+                  <p>Última atualização: ${repo.updated_at}</p>
+                  <p>Branch: ${repo.defalt_branch}</p>
+                  <p>Descrição: ${repo.description}</p>
+                  <p>Linguagem: ${repo.language}</p>
+                  <p>Visibilidade: ${repo.visibility}</p>
+                </div>
+            `
+            document.querySelector('.repos').innerHTML = ctx
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
     infosFn(input.value)
+    reposFn(input.value)
 })
+    
