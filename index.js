a6066638a82d79170b5cc75a6bfc03e17ee3b812
isabellyruhamah <isabellyruhamah@gmail.com>
2025-04-02 11:30:09 -0300

    Coclusão

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const infosFn = async (nome) => {
                     <p>${data.login}</p>
                 </div>
                 <div>
-                    <p>Bio: ${data.bio}</p>
+                    
+                    <p>${data.bio ? `Bio: ${data.bio}`:''}</p>
+
                     <p>Seguidores: ${data.followers} | Seguindo: ${data.following}</p>
                     <p>Repositórios públicos: ${data.public_repos}</p>
                     <a href="${data.html_url}" target="_blank">Acesse o perfil</a>
@@ -39,15 +41,21 @@ const reposFn = async (nome) => {
                 <div class = "repo">
                   <h2>${repo.name}</h2>
                   <hr>
-                  <p>Permite fork${repo.allow_forking}
-                  </p>
-                  <p>URL para clone ${repo.clone_URL}</p>
-                  <p>Criado em: ${repo.create_at}</p>
-                  <p>Última atualização: ${repo.updated_at}</p>
+                  <p>${repo.allow_forking ? 'Permite fork' : 'Não permite fork'}</p>
+
+                  <p><a href="${repo.clone_URL}">URL para clone<a></p>
+
+                  <p>Data de criação: ${new Date(repo.created_at).toLocaleDateString('pt-br')}</p>
+
+                  <p>Última atualização: ${new Date(repo.updated_at).toLocaleDateString('pt-br')}</p>
+
                   <p>Branch: ${repo.defalt_branch}</p>
-                  <p>Descrição: ${repo.description}</p>
-                  <p>Linguagem: ${repo.language}</p>
-                  <p>Visibilidade: ${repo.visibility}</p>
+
+                  <p>${repo.description != null ? `Descrição: ${repo.description}` : 'Sem descrição'}<p> 
+
+                  <p>${repo.language != null ?`Linguagem: ${repo.language}` : 'Sem descrição'}</p>
+
+                  <p>${repo.visibility === 'public' ? 'Repositório público' : 'Repositório privado'}</p>
                 </div>
             `
             document.querySelector('.repos').innerHTML = ctx
